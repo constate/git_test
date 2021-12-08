b36e6bb712290b641ea4d98542634df7e5fa232c
sunghyun Kim <constate@naver.com>
2021-12-08 11:11:26 +0900

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+import express from "express";
+import http from "http";
+const app = express();
+
 console.log("nodejs server test");
+app.get("/", (req, res) => {
+  res.end("<h1>Hello world</h1>");
+});
+
+app.get("/car/input", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html; charset" });
+  res.write("<h2>자동차 목록</h2>");
+  res.end();
+});
+
+const server = http.createServer(3000, () => {
+  console.log("on server port 3000");
+});
